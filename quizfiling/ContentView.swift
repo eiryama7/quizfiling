af9e894f442dd705ec49405fb1d2b6ab9975bac9
eiryama7 <pokemonnharimaronn@gmail.com>
2025-12-27 21:45:20 +0900

    Initial Commit

---
+++ quizfiling/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  quizfiling
+//
+//  Created by 難波映太 on 2025/12/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ quizfiling/quizfilingApp.swift
@@ -0,0 +1,17 @@
+//
+//  quizfilingApp.swift
+//  quizfiling
+//
+//  Created by 難波映太 on 2025/12/27.
+//
+
+import SwiftUI
+
+@main
+struct quizfilingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
